2efb52d277c01415877baab4b0a745f2722d6874
ambunovo <ambu4ever@gmail.com>
2012-12-17 04:23:02 +0200

    Greetingspopup
    pakeisti mygtukai,tekstas. dar siektiek paturbinta. pats i servaimetu

--- novaturas/application/views/greeting.php
+++ novaturas/application/views/greeting.php
@@ -9,22 +9,16 @@
     
     <div id="greetingPopup" class="greeting with-overlay popup">
 		<h2><span>"Novaturas"</span></h2>
-                <p><?php echo $s['other']['greetin-text'] ?></p>
-		<p class="author">
-                    2012-12-24, Novaturas<br />
-        <a href="#" onclick="restartAnimation(); return false" class="simple-button">
-            <?php echo $s['greeting']['repeat-animation'] ?>
-        </a>
-        <a href="<?php echo base_url().$lang ?>/create" class="simple-button">
-            <?php echo $s['greeting']['create-your-own'] ?>
-        </a>
-                    </p>
-    </div><!-- end of #greetingPopup -->
-    <footer style="height:48px"></footer>
+		<p><?php echo $s['other']['greetin-text'] ?></p>
+		<p class="author">Novaturas</p>
+		<div class="buttons-container">
+			<a href="#" onclick="restartAnimation(); return false" class="simple-button b-repeat"><span></span>
+				<?php echo $s['greeting']['repeat-animation'] ?>
+			</a>
+			<a href="<?php echo base_url().$lang ?>/create" class="simple-button b-create"><span></span>
+				<?php echo $s['greeting']['create-your-own'] ?>
+			</a>
+		</div>
+	</div><!-- end of #greetingPopup -->
 </div><!--! end of #greeting -->
-
-<!--<div id="greetingPopup" class="popup with-overlay">
-        
-</div><!--! end of #greetingPopup -->
-
 <script src="<?php echo base_url() ?>js/greeting.js"type="text/javascript"></script>
